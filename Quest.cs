1c092ef141ddf10c1f49217e094678e7adef0ea2
Kamish Ziqing Wang <kamishwang@gmail.com>
2024-11-03 13:57:49 -0800

    Update Quest.cs

--- Quest.cs
+++ Quest.cs
@@ -1,1 +1,29 @@
+public class DayNightManager : MonoBehaviour
+{
+    private int weekday = 1;
+    private bool isDay = true;
+
+    public int GetWeekday()
+    {
+        return weekday;
+    }
+
+    public bool GetIsDay()
+    {
+        return isDay;
+    }
+
+    public void ProgressDay()
+    {
+        if (isDay)
+        {
+            isDay = false;
+        }
+        else
+        {
+            isDay = true;
+            weekday = (weekday % 7) + 1;
+        }
+    }
+}
 
